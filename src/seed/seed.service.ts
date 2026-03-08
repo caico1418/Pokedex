7b5a8dc1bc9f5adff2f9009322a8630a5afb1c73
Edgar Gonzalez <glez77104@gmail.com>
2026-03-07 22:34:38 -0600

    Se mejora la insercion de data multiple en la DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,16 +17,24 @@ export class SeedService {
 
   
   async executeSeed() {
+
+    await this.pokemonModel.deleteMany({});
+
     const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
-    data.results.forEach( async ({ name, url }) => {
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
 
-      const pokemon = await this.pokemonModel.create({ name, no });
+      //const pokemon = await this.pokemonModel.create({ name, no });
 
+      pokemonToInsert.push({ name, no });
     })
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
     return 'Seed Executed';
   }
