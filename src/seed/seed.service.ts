e48d7a1418d0328f6790a9aca0b60faee99249ab
Edgar Gonzalez <glez77104@gmail.com>
2026-03-07 21:55:03 -0600

    Se agrega una interfaz para la data de la api

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios, {AxiosInstance} from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
   
-  executeSeed() {
-    return 'executed SEED';
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    
+
+    return data.results;
   }
 }
