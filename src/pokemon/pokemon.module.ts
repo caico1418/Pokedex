7dc5848618ab701816f100e81e4856fb75526b5e
Edgar Gonzalez <glez77104@gmail.com>
2026-03-07 22:13:12 -0600

    Se termina la implementacion de SEED para crear la data en la db

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,10 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
         schema: PokemonSchema,
       }
     ])
+  ],
+
+  exports: [
+    MongooseModule
   ]
 })
 export class PokemonModule {}
